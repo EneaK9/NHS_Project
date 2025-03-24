d5e395d4a7c23369fdb2a95d28e1c06f7a45743a
Enea Kuca <eneakuca0@gmail.com>
2025-03-24 23:01:00 +0100

    DB error fix

--- backend/database/db_connection.py
+++ backend/database/db_connection.py
@@ -3,9 +3,9 @@ import os
 
 def get_db_connection():
     return psycopg2.connect(
-        host=os.getenv("DB_HOST"),
-        port=os.getenv("DB_PORT"),
-        database=os.getenv("DB_NAME"),
-        user=os.getenv("DB_USER"),
-        password=os.getenv("DB_PASSWORD")
+        host=os.environ.get("DB_HOST"),
+        database=os.environ.get("DB_NAME"),
+        user=os.environ.get("DB_USER"),
+        password=os.environ.get("DB_PASSWORD"),
+        port=os.environ.get("DB_PORT", 5432)  # default PostgreSQL port
     )
