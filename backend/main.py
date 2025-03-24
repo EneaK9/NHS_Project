c035c68c1289d30378a3a43d870f13eeba842b0b
Enea Kuca <eneakuca0@gmail.com>
2025-03-25 00:02:56 +0100

    DB error fix 4.99

--- backend/main.py
+++ backend/main.py
@@ -1,3 +1,7 @@
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from flask import Flask
 from flask_cors import CORS
 from routes.conditions import conditions_bp  # Import the blueprint for API routes
