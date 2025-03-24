e97eda88ab197bef6926d243e46edb0f2cb959ec
Enea Kuca <eneakuca0@gmail.com>
2025-03-24 23:37:11 +0100

    DB error fix 2

--- backend/database/db.sql
+++ backend/database/db.sql
@@ -1,14 +1,20 @@
-CREATE TABLE IF NOT EXISTS conditions_albanian (
+-- DROP TABLES IF EXIST
+DROP TABLE IF EXISTS condition_sections_albanian;
+DROP TABLE IF EXISTS conditions_albanian;
+
+-- CREATE conditions_albanian table
+CREATE TABLE conditions_albanian (
   id SERIAL PRIMARY KEY,
   condition_name VARCHAR(255) NOT NULL,
-  condition_slug VARCHAR(255) UNIQUE NOT NULL,
+  condition_slug VARCHAR(255) NOT NULL UNIQUE,
   url VARCHAR(500),
   full_json TEXT,
   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
-CREATE TABLE IF NOT EXISTS condition_sections_albanian (
+-- CREATE condition_sections_albanian table
+CREATE TABLE condition_sections_albanian (
   id SERIAL PRIMARY KEY,
   condition_slug VARCHAR(255) NOT NULL,
   section_name VARCHAR(255) NOT NULL,
