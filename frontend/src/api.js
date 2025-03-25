d3f58b77d4e504d0fe5b8ab111036b051de8985c
Enea Kuca <eneakuca0@gmail.com>
2025-03-25 15:32:19 +0100

    Fixing api

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -4,7 +4,7 @@ const API_URL = "https://nhs-project.onrender.com"; // New API endpoint
 
 export const fetchData = async () => {
   try {
-    const response = await axios.get(API_URL);
+    const response = await axios.get(`${API_URL}/api/conditions`);
     return response.data;
   } catch (error) {
     console.error("Error fetching data:", error);
