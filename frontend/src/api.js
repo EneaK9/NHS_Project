e3d44bc81ce0d11be01cd943b1f0551687ef51e1
Enea Kuca <eneakuca0@gmail.com>
2025-03-25 16:49:07 +0100

    Exporting the data to the db 2

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,15 +1,13 @@
 import axios from "axios";
 
-const API_URL = "https://nhs-project.onrender.com"; // New API endpoint
+const API_URL = "https://nhs-project.onrender.com/api";
 
 export const fetchData = async () => {
   try {
-    const response = await axios.get(`${API_URL}/api/conditions`);
+    const response = await axios.get(`${API_URL}/conditions`);
     return response.data;
   } catch (error) {
     console.error("Error fetching data:", error);
     throw error;
   }
 };
-
-
